f317c92db536ff1d2a1ca5c9d67f5abcd2b7a43e
RickySandi <rickysandis@gmail.com>
2020-09-27 00:08:25 -0400

    Beer Service solucionado

--- BreweryAPI/BreweryAPI/Services/BeersService.cs
+++ BreweryAPI/BreweryAPI/Services/BeersService.cs
@@ -1,10 +1,76 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BreweryAPI.Data.Entities;
+using BreweryAPI.Data.Repository;
+using BreweryAPI.Exceptions;
+using BreweryAPI.Models;
+
 namespace BreweryAPI.Services
 {
-    public class BeersService
+    public class BeersService : IBeersService
     {
-        public BeersService()
+        private IMapper _mapper;
+        private ILibraryRepository _libraryRepository;
+
+        public BeersService(IMapper mapper, ILibraryRepository libraryRepository)
         {
+            _mapper = mapper;
+            _libraryRepository = libraryRepository;
         }
+
+        public BeerModel CreateBeer(int breweryId, BeerModel beer)
+        {
+            validateBrewery(breweryId);
+            return _mapper.Map<BeerModel>(_libraryRepository.CreateBeer(_mapper.Map<BeerEntity>(beer)));
+        }
+
+        public bool DeleteBeer(int breweryId, int beerId)
+        {
+            GetBeer(breweryId, beerId);
+            return _libraryRepository.DeleteBeer(beerId);
+        }
+
+        public BeerModel GetBeer(int breweryId, int beerId)
+        {
+            validateBrewery(breweryId); 
+            validateBeer(beerId);
+            return _mapper.Map<BeerModel>(_libraryRepository.GetBeer(beerId));
+        }
+
+        public IEnumerable<BeerModel> GetBeers(int breweryId)
+        {
+            validateBrewery(breweryId);
+            return _mapper.Map<IEnumerable<BeerModel>>(_libraryRepository.GetBeers(breweryId));
+
+        }
+
+        public BeerModel UpdateBeer(int breweryId, int beerId, BeerModel beer)
+        {
+            GetBeer(breweryId, beerId);
+            beer.Id = beerId;
+            return _mapper.Map<BeerModel>(_libraryRepository.UpdateBeer(_mapper.Map<BeerEntity>(beer)));
+        }
+
+        private void validateBrewery(int breweryId)
+        {
+            var brewery = _libraryRepository.GetBeer(breweryId);
+            if (brewery == null)
+            {
+                throw new NotFoundOperationException($"the brewery id:{breweryId}, does not exist");
+            }
+        }
+
+        private void validateBeer(int beerId)
+        {
+            var beer = _libraryRepository.GetBeer(beerId);
+            if (beer == null)
+            {
+                throw new NotFoundOperationException($"the beer id:{beerId}, does not exist");
+            }
+        }
+
     }
 }
--- BreweryAPI/BreweryAPI/Services/BreweriesService.cs
+++ BreweryAPI/BreweryAPI/Services/BreweriesService.cs
@@ -1,22 +1,97 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BreweryAPI.Data.Entities;
+using BreweryAPI.Data.Repository;
+using BreweryAPI.Exceptions;
 using BreweryAPI.Models;
+
 
 namespace BreweryAPI.Services
 {
     public class BreweriesService : IBreweriesService
     {
-        IEnumerable<BreweryModel> GetBreweries()
+        ILibraryRepository _libraryRepository;
+        private IMapper _mapper;
+
+        private HashSet<string> allowedOrderByParameters = new HashSet<string>()
         {
-            throw new NotImplementedException();
+            "id",
+            "name",
+            "country",
+            "fundation-date"
+            
+        };
+
+        public BreweriesService(ILibraryRepository libraryRepository, IMapper mapper)
+        {
+            _libraryRepository = libraryRepository;
+            _mapper = mapper;
         }
 
-        BreweryModel GetBrewery(int companyId)
+        public BreweryModel CreateBrewery(BreweryModel breweryModel)
         {
-            throw new NotImplementedException();
+
+            var breweryEntity = _mapper.Map<BreweryEntity>(breweryModel);
+            var returnedBreuery = _libraryRepository.CreateBrewery(breweryEntity);
+            return _mapper.Map<BreweryModel>(returnedBreuery);
         }
 
+        public DeleteModel DeleteBrewery(int breweryId)
+        {
+            var companyToDelete = GetBrewery(breweryId);
+
+            var result = _libraryRepository.DeleteBrewery(breweryId);
+
+            if (result)
+            {
+                return new DeleteModel()
+                {
+                    IsSuccess = result,
+                    Message = "The brewery was deleted."
+                };
+            }
+            else
+            {
+                return new DeleteModel()
+                {
+                    IsSuccess = result,
+                    Message = "The brewery was not deleted."
+                };
+            }
+
+        }
+
+        public IEnumerable<BreweryModel> GetBreweries(string orderBy)
+        {
+            if (!allowedOrderByParameters.Contains(orderBy.ToLower()))
+            {
+                throw new BadRequestOperationException($"the field: {orderBy} is not supported, please use one of these {string.Join(",", allowedOrderByParameters)}");
+            }
+
+            var entityList = _libraryRepository.GetBreweries(orderBy);
+            var modelList = _mapper.Map<IEnumerable<BreweryModel>>(entityList);
+            return modelList;
+        }
+
+        public BreweryModel GetBrewery(int breweryId)
+        {
+            var brewery = _libraryRepository.GetBrewery(breweryId);
+            if (brewery == null)
+            {
+                throw new NotFoundOperationException($"The brewery with id:{breweryId} does not exists");
+            }
+
+            return _mapper.Map<BreweryModel>(brewery);
+        }
+
+        public BreweryModel UpdateBrewery(int breweryId, BreweryModel breweryModel)
+        {
+            var breweryEntity = _mapper.Map<BreweryEntity>(breweryModel);
+            _libraryRepository.UpdateBrewery(breweryEntity);
+            return breweryModel;
+        }
     }
 }
